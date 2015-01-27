7da52aa1e75fe989ba42ea0defb46bb5f11d6996
bradlys <bradlyschlenker@gmail.com>
2015-01-26 20:12:11 -0700

    UI changes
    A long journey ahead

--- public/javascripts/danceControllers.js
+++ public/javascripts/danceControllers.js
@@ -3,14 +3,20 @@ var danceControllers = angular.module('danceControllers', []);
 danceControllers.controller('DanceListCtrl', function($scope, dances){
 	dances.list(function(dances) {
 		$scope.dances = dances;
-		var styles = [""];
+		var styles = ["All Dances"];
+		var days = ["All Days"];
 		for(var dance in dances){
 			if(styles.indexOf(dances[dance].style) === -1){
 				styles.push(dances[dance].style);
 			}
+			if(days.indexOf(dances[dance].reoccurringDate) === -1){
+				days.push(dances[dance].reoccurringDate);
+			}
 		}
 		$scope.styles = styles;
-		$scope.styleFilter = "";
+		$scope.days = days;
+		$scope.styleFilter = "All Dances";
+		$scope.dayFilter = "All Days";
 	});
 });
 
--- public/javascripts/danceFilters.js
+++ public/javascripts/danceFilters.js
@@ -1,4 +1,6 @@
-angular.module('danceFilters', []).filter('reoccurringDateFormatter', function() {
+var app = angular.module('danceFilters', []);
+
+app.filter('reoccurringDateFormatter', function() {
   return function(input) {
     if(input == false){
     	return "false";
@@ -6,4 +8,36 @@ angular.module('danceFilters', []).filter('reoccurringDateFormatter', function()
     	return "every ... " + input;
     }
   };
+});
+
+app.filter('stylesFilter', function(){
+	return function(dances, style) {
+		if(style === "All Dances"){
+			return dances;
+		}
+		var filtered = [];
+		angular.forEach(dances, function(dance) {
+	      if(style === dance.style) {
+	        filtered.push(dance);
+	      }
+	    });
+    	return filtered;
+	}
+});
+
+//Holding place, totally incorrect.
+//This requires a bit more research
+app.filter('daysFilter', function(){
+	return function(dances, day){
+		if(day === "All Days"){
+			return dances;
+		}
+		var filtered = [];
+		angular.forEach(dances, function(dance) {
+	      if(day === dance.reoccurringDate) {
+	        filtered.push(dance);
+	      }
+	    });
+    	return filtered;
+	}
 });
